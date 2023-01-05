5c873db048a6200eb825ba74b3941f9678391097
22P31A0425 <119719586+22P31A0425@users.noreply.github.com>
2023-01-05 11:15:38 +0530

    #include<stdio.h> int main() {     int wd;     scanf("%d",&wd);     if(wd==1)     {         printf("monday");
        }
        else
        if(wd==2)
        {
            printf("tuesday");
            
        }
        else
        if(wd==3)
        {
            printf("wednesday");
            
        }
        else
        if(wd==4)
        {
            printf("thursday");
            
        }
        else
        if(wd==5)
        {
            printf("friday");
            
        }
        else
        if(wd==6)
        {
            printf("saturday");
            
        }
        else
        if(wd==7)
        {
            printf("sunday");
            
        }
        else
        {
            printf("invalid input");
            
        }
        
    }

---
+++ Day_name__.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+    int wd;
+    scanf("%d",&wd);
+    if(wd==1)
+    {
+        printf("monday");
+        
+    }
+    else
+    if(wd==2)
+    {
+        printf("tuesday");
+        
+    }
+    else
+    if(wd==3)
+    {
+        printf("wednesday");
+        
+    }
+    else
+    if(wd==4)
+    {
+        printf("thursday");
+        
+    }
+    else
+    if(wd==5)
+    {
+        printf("friday");
+        
+    }
+    else
+    if(wd==6)
+    {
+        printf("saturday");
+        
+    }
+    else
+    if(wd==7)
+    {
+        printf("sunday");
+        
+    }
+    else
+    {
+        printf("invalid input");
+        
+    }
+    
+}
